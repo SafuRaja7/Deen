5e853bcf84635afb77d890b5eb8440277cf9967f
SafuRaja7 <rajasaifi49@gmail.com>
2026-01-02 16:03:07 +0500

    refactor: Replace dynamic last read card subtitle and navigation logic with static placeholders.

--- lib/features/quran/presentation/widgets/quran_body.dart
+++ lib/features/quran/presentation/widgets/quran_body.dart
@@ -48,28 +48,8 @@ class QuranBody extends StatelessWidget {
                           flex: 1,
                           child: PrevRecordCard(
                             title: "Last Read",
-                            subtitle: state.lastReadSurahName != null
-                                ? '${state.lastReadSurahName} ${state.lastReadAyahNumber}'
-                                : "None",
-                            onTap: () {
-                              if (state.lastReadSurahNumber != null) {
-                                Navigator.pushNamed(
-                                  context,
-                                  AppRoutes.surahDetails,
-                                  arguments: {
-                                    'surahNumber': state.lastReadSurahNumber,
-                                    'initialAyahNumberInSurah':
-                                        state.lastReadAyahNumber,
-                                  },
-                                ).then((_) {
-                                  if (context.mounted) {
-                                    context.read<QuranBloc>().add(
-                                      LoadQuranData(),
-                                    );
-                                  }
-                                });
-                              }
-                            },
+                            subtitle: "Al Bakarah 117",
+                            onTap: () {},
                           ),
                         ),
                         Flexible(
